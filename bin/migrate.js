f7837096ccdec5bc95848dca79209b98305cbdca
darren.haken <darrenhaken@gmail.com>
2013-10-23 14:25:29 +0100

    Fixed the create migration command so that it prefixes the file with the number of the script such as 001, 002.

--- bin/migrate.js
+++ bin/migrate.js
@@ -3,60 +3,23 @@
 /**
  * Module dependencies.
  */
-
 var migrate = require('../lib/migrate'),
     log = require('../lib/logger'),
     join = require('path').join,
     fs = require('fs'),
     program = require('commander');
 
-var args = process.argv.slice(2);
 var options = { args: [] };
-var currentWorkingDirectory = process.cwd();
-
-// require an argument
-
-function required() {
-    if (args.length) return args.shift();
-    abort(arg + ' requires an argument');
-}
 
 // abort with a message
-
 function abort(msg) {
     console.error('  %s', msg);
     process.exit(1);
 }
 
-// parse arguments
-
-var arg;
-while (args.length) {
-    arg = args.shift();
-    switch (arg) {
-        case '-h':
-        case '--help':
-        case 'help':
-            console.log(usage);
-            process.exit();
-            break;
-        case '-c':
-        case '--chdir':
-            process.chdir(currentWorkingDirectory = required());
-            break;
-        default:
-            if (options.command) {
-                options.args.push(arg);
-            } else {
-                options.command = arg;
-            }
-    }
-}
-
 /**
  * Load migrations.
  */
-
 function migrations() {
     return fs.readdirSync('migrations').filter(function (file) {
         return file.match(/^\d+.*\.js$/);
@@ -65,30 +28,10 @@ function migrations() {
         });
 }
 
-/**
- * Slugify the given `str`.
- */
-
-function slugify(str) {
-    return str.replace(/\s+/g, '-');
-}
-
-// create ./migrations
-
-try {
-    fs.mkdirSync('migrations', 0774);
-} catch (err) {
-    // ignore
-}
-
-// commands
-
 var commands = {
-
     /**
      * up [name]
      */
-
     up: function (migrationName) {
         performMigration('up', migrationName);
     },
@@ -96,41 +39,10 @@ var commands = {
     /**
      * down [name]
      */
-
     down: function (migrationName) {
         performMigration('down', migrationName);
-    },
-
-    /**
-     * create [title]
-     */
-
-    create: function () {
-        var migrations = fs.readdirSync('migrations').filter(function (file) {
-            return file.match(/^\d+/);
-        }).map(function (file) {
-                return parseInt(file.match(/^(\d+)/)[1], 10);
-            }).sort(function (a, b) {
-                return a - b;
-            });
-
-        var curr = pad((migrations.pop() || 0) + 1)
-            , title = slugify([].slice.call(arguments).join(' '));
-        title = title ? curr + '-' + title : curr;
-        create(title);
     }
 };
-
-/**
- * Pad the given number.
- *
- * @param {Number} n
- * @return {String}
- */
-
-function pad(n) {
-    return Array(4 - n.toString().length).join('0') + n;
-}
 
 /**
  * Perform a migration in the given `direction`.
--- bin/node-migrate.js
+++ bin/node-migrate.js
@@ -4,11 +4,11 @@ var migrate = require('../lib/migrate'),
     log = require('../lib/logger'),
     join = require('path').join,
     fs = require('fs'),
-    program = require('commander'),
-    packageJson = ('./package.json');
+    program = require('commander');
 
 program
     .version('0.1.3')
+    .option('-C, --chdir [directory]', 'Specify working directory to process migrations', process.cwd())
     .option('-c, --connection', 'specify the MongoDB connection')
     .option('-u, --username', 'specify the MongoDB Username')
     .option('-p, --password', 'specify the MongoDB Password');
@@ -20,10 +20,8 @@ program
 
 program
     .command('migrate')
-    .description('create migration script')
-    .action(function(){
-        console.log('migrate:');
-    });
+    .description('migrate MongoDB instance')
+    .action(migrateDatabase);
 
 //Must execute this after registering all the commands
 program.parse(process.argv);
@@ -39,29 +37,52 @@ function create(migrationName) {
         }
     }
 
+    function pad(n) {
+        return Array(4 - n.toString().length).join('0') + n;
+    }
+
+    function createMigrationTitle(migrationName) {
+        var migrations = fs.readdirSync('migrations')
+            .filter(function(file){
+                return file.match(/^\d+/);
+            })
+            .map(function(file){
+                return parseInt(file.match(/^(\d+)/)[1], 10);
+            })
+            .sort(function(a, b){
+                return a - b;
+            });
+
+        var migrationNumber = pad((migrations.pop() || 0) + 1);
+        return migrationNumber + '-' + migrationName;
+    }
+
     function createMigrationTemplate(migrationName) {
-        var path = 'migrations/' + migrationName + '.js',
-            currentWorkingDirectory = process.cwd();
+        var migrationFileName = createMigrationTitle(migrationName);
 
-        var template = [
-            '',
-            'exports.up = function(next){',
-            '  next();',
-            '};',
-            '',
-            'exports.down = function(next){',
-            '  next();',
-            '};',
-            ''
-        ].join('\n');
+        var path = 'migrations/' + migrationFileName + '.js',
+            template = [
+                '',
+                'exports.up = function(next){',
+                '  next();',
+                '};',
+                '',
+                'exports.down = function(next){',
+                '  next();',
+                '};',
+                ''
+            ].join('\n');
 
-        log('create', join(currentWorkingDirectory, path));
-        fs.writeFileSync(path, template);
+        var fullPath = join(program.chdir, path);
+
+        log('create', fullPath);
+        fs.writeFileSync(fullPath, template);
     }
 
     createMigrationDirectory();
     createMigrationTemplate(migrationName);
 }
 
+function migrateDatabase() {
 
-
+}
